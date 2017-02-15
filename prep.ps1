720fa1e1acb2d022ac1e61f7a01177288889b343
ctrlbold <clemaire@gmail.com>
2017-02-15 01:43:35 +0100

    sure, why not

---
+++ prep.ps1
@@ -0,0 +1,9 @@
+﻿# open google on a specific page
+Get-Process *skype* | Stop-Process
+Get-Process *pidgin* | Stop-Process
+Get-Process *slack* | Stop-Process
+
+# Copy-SqlSpConfigure
+Copy-SqlSpConfigure -Source sql2008 -Destination sql2016 -Configs DefaultBackupCompression, IsSqlClrEnabled
+
+Set-DbaMaxMemory sql2016\vnext -MaxMb 1024
