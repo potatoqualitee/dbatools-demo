b78433ba4f9d26be866093cb0e466ea37445cc76
ctrlbold <clemaire@gmail.com>
2017-04-30 13:58:47 +0200

    updates for psconf.eu

--- dbatools-admin-bestpractice.ps1
+++ dbatools-admin-bestpractice.ps1
@@ -12,66 +12,36 @@ $env:PSModulePath -Split ";"
 Import-Module C:\github\dbatools -Force
 
 # Set some vars
-$new = "sql2016\vnext"
-$old = $instance = "sql2014"
-$coupleservers = "sql2012","sql2016"
-$allservers = "sql2008","sql2012","sql2014","sql2016","sqlcluster","sql2005"
-
-# MY NEW TRIck (thanks @alexandair, et al)
-break
-
-#region configs
-
-# Get-DbaSpConfigure - @sirsql
-$oldprops = Get-DbaSpConfigure -SqlServer $old
-$newprops = Get-DbaSpConfigure -SqlServer $new
-
-$propcompare = foreach ($prop in $oldprops) {
-    [pscustomobject]@{
-    Config = $prop.DisplayName
-    'SQL Server 2014' = $prop.RunningValue
-    'SQL Server 2016' = $newprops | Where ConfigName -eq $prop.ConfigName | Select -ExpandProperty RunningValue
-    }
-} 
-
-$propcompare | Out-GridView
-
-# Copy-SqlSpConfigure
-Copy-SqlSpConfigure -Source $old -Destination $new -Configs DefaultBackupCompression, IsSqlClrEnabled
-
-# Get-DbaSpConfigure - @sirsql
-Get-DbaSpConfigure -SqlServer $new | Where-Object { $_.ConfigName -in 'DefaultBackupCompression', 'IsSqlClrEnabled' } | 
-Select-Object ConfigName, RunningValue, IsRunningDefaultValue | Format-Table -AutoSize
-
-#endregion
+$new = "localhost\sql2016"
+$old = $instance = "localhost"
 
 #region backuprestore
 
 Start-Process https://dbatools.io/snowball
 
 # standard
-Restore-DbaDatabase -SqlServer localhost -Path C:\temp\SQL2016_Cube_Query_History_FULL_20170206_115448.bak
-Restore-DbaDatabase -SqlServer localhost -Path C:\temp\SQL2016_Cube_Query_History_FULL_20170206_115448.bak -WithReplace
+Restore-DbaDatabase  -SqlInstance localhost -Path C:\temp\SQL2016_Cube_Query_History_FULL_20170206_115448.bak
+Restore-DbaDatabase  -SqlInstance localhost -Path C:\temp\SQL2016_Cube_Query_History_FULL_20170206_115448.bak -WithReplace
 
 # ola!
 Invoke-Item \\nas\sql\SQL2016\db1
-Restore-DbaDatabase -SqlServer sql2016 -Path \\nas\sql\SQL2016\db1 -WithReplace -DestinationDataDirectory C:\temp
+Restore-DbaDatabase  -SqlInstance sql2016 -Path \\nas\sql\SQL2016\db1 -WithReplace -DestinationDataDirectory C:\temp
 
 foreach ($database in (Get-ChildItem -Directory \\nas\sql\SQL2016).FullName)
 {
     Write-Output "Processing $database"
-    Restore-DbaDatabase -SqlServer sql2016\vnext -Path $database -NoRecovery # -RestoreTime (Get-date).AddHours(-3)
+    Restore-DbaDatabase  -SqlInstance localhost\sql2016 -Path $database -NoRecovery # -RestoreTime (Get-date).AddHours(-3)
 }
 
 # What about backups?
 Get-DbaDatabase -SqlInstance sql2005 -Databases db_2005_CL80 | Backup-DbaDatabase -BackupDirectory C:\temp -NoCopyOnly
 
 # history
-Get-DbaBackupHistory -SqlServer sql2005 -Databases dumpsterfire4, db_2005_CL80 | Out-GridView
+Get-DbaBackupHistory  -SqlInstance sql2005 -Databases dumpsterfire4, db_2005_CL80 | Out-GridView
 
 # backup header
-Read-DbaBackupHeader -SqlServer $instance -Path "\\nas\sql\SQL2016\db1\FULL\SQL2016_db1_FULL_20170206_115448.bak"
-Read-DbaBackupHeader -SqlServer $instance -Path "\\nas\sql\SQL2016\db1\FULL\SQL2016_db1_FULL_20170206_115448.bak" | SELECT ServerName, DatabaseName, UserName, BackupFinishDate, SqlVersion, BackupSizeMB
+Read-DbaBackupHeader  -SqlInstance $instance -Path "\\nas\sql\SQL2016\db1\FULL\SQL2016_db1_FULL_20170206_115448.bak"
+Read-DbaBackupHeader  -SqlInstance $instance -Path "\\nas\sql\SQL2016\db1\FULL\SQL2016_db1_FULL_20170206_115448.bak" | SELECT ServerName, DatabaseName, UserName, BackupFinishDate, SqlVersion, BackupSizeMB
 
 # Find it!
 Find-DbaCommand -Tag Config
@@ -95,7 +65,7 @@ $allservers | Get-DbaLastBackup | Where-Object LastFullBackup -eq $null | Format
 $allservers | Get-DbaLastBackup | Where-Object { $_.SinceLog -gt '00:15:00' -and $_.RecoveryModel -ne 'Simple' -and $_.Database -ne 'model' } | Select Server, Database, SinceFull, DatabaseCreated | Out-GridView
 
 # LastGoodCheckDb - by @jagoop
-$checkdbs = Get-DbaLastGoodCheckDb -SqlServer $instance
+$checkdbs = Get-DbaLastGoodCheckDb  -SqlInstance $instance
 $checkdbs
 $checkdbs | Where LastGoodCheckDb -eq $null
 $checkdbs | Where LastGoodCheckDb -lt (Get-Date).AddDays(-1)
@@ -113,8 +83,8 @@ Get-Help Test-DbaLastBackup -Online
 Import-Module SqlServer
 Invoke-Item (Get-Item SQLSERVER:\SQL\LOCALHOST\DEFAULT).DefaultFile
 
-Test-DbaLastBackup -SqlServer sql2005 | Out-GridView
-Test-DbaLastBackup -SqlServer sql2005 -Destination sql2016\vnext -VerifyOnly | Out-GridView
+Test-DbaLastBackup  -SqlInstance sql2005 | Out-GridView
+Test-DbaLastBackup  -SqlInstance sql2005 -Destination localhost\sql2016 -VerifyOnly | Out-GridView
 
 #endregion
 
@@ -127,9 +97,9 @@ $allservers | Test-DbaVirtualLogFile | Where-Object {$_.Count -ge 50} | Sort-Obj
 #region databasespace
 
 # Get Db Free Space AND write it to disk
-Get-DbaDatabaseFreespace -SqlServer $instance
-Get-DbaDatabaseFreespace -SqlServer $instance -IncludeSystemDBs | Out-DbaDataTable | Write-DbaDataTable -SqlServer $instance -Table tempdb.dbo.DiskSpaceExample
-Get-DbaDatabaseFreespace -SqlServer $instance -IncludeSystemDBs | Out-DbaDataTable | Write-DbaDataTable -SqlServer $instance -Table tempdb.dbo.DiskSpaceExample -AutoCreateTable
+Get-DbaDatabaseFreespace  -SqlInstance $instance
+Get-DbaDatabaseFreespace  -SqlInstance $instance -IncludeSystemDBs | Out-DbaDataTable | Write-DbaDataTable  -SqlInstance $instance -Table tempdb.dbo.DiskSpaceExample
+Get-DbaDatabaseFreespace  -SqlInstance $instance -IncludeSystemDBs | Out-DbaDataTable | Write-DbaDataTable  -SqlInstance $instance -Table tempdb.dbo.DiskSpaceExample -AutoCreateTable
 
 # Run a lil query
 Ssms.exe "C:\temp\tempdbquery.sql"
@@ -142,19 +112,19 @@ Ssms.exe "C:\temp\tempdbquery.sql"
 $allservers | Get-DbaMaxMemory
 $allservers | Test-DbaMaxMemory | Format-Table
 $allservers | Test-DbaMaxMemory | Where-Object { $_.SqlMaxMB -gt $_.TotalMB } | Set-DbaMaxMemory -WhatIf
-Set-DbaMaxMemory -SqlServer $instance -MaxMb 2048
+Set-DbaMaxMemory  -SqlInstance $instance -MaxMb 2048
 
 # RecoveryModel
-Test-DbaFullRecoveryModel -SqlServer sql2005
-Test-DbaFullRecoveryModel -SqlServer sql2005 | Where { $_.ConfiguredRecoveryModel -ne $_.ActualRecoveryModel }
+Test-DbaFullRecoveryModel  -SqlInstance sql2005
+Test-DbaFullRecoveryModel  -SqlInstance sql2005 | Where { $_.ConfiguredRecoveryModel -ne $_.ActualRecoveryModel }
 
 # Backup History!
-Get-DbaBackupHistory -SqlServer $instance
-Get-DbaBackupHistory -SqlServer $instance | Out-GridView
-Get-DbaBackupHistory -SqlServer $instance -Databases AdventureWorks2012 | Format-Table -AutoSize
+Get-DbaBackupHistory  -SqlInstance $instance
+Get-DbaBackupHistory  -SqlInstance $instance | Out-GridView
+Get-DbaBackupHistory  -SqlInstance $instance -Databases AdventureWorks2012 | Format-Table -AutoSize
 
 # Restore History!
-Get-DbaRestoreHistory -SqlServer $instance | Out-GridView
+Get-DbaRestoreHistory  -SqlInstance $instance | Out-GridView
  
 #endregion
 
@@ -166,22 +136,47 @@ $allservers | Find-DbaStoredProcedure -Pattern dbatools | Select * | Out-GridVie
 $coupleservers | Find-DbaStoredProcedure -Pattern '\w+@\w+\.\w+'
 
 # Remove dat orphan - by @sqlstad
-Find-DbaOrphanedFile -SqlServer $instance
-((Find-DbaOrphanedFile -SqlServer $instance -RemoteOnly | Get-ChildItem | Select -ExpandProperty Length | Measure-Object -Sum)).Sum / 1MB
-Find-DbaOrphanedFile -SqlServer $instance -RemoteOnly | Remove-Item
+Find-DbaOrphanedFile  -SqlInstance $instance
+((Find-DbaOrphanedFile  -SqlInstance $instance -RemoteOnly | Get-ChildItem | Select -ExpandProperty Length | Measure-Object -Sum)).Sum / 1MB
+Find-DbaOrphanedFile  -SqlInstance $instance -RemoteOnly | Remove-Item
 
 # Reset-SqlAdmin
-Reset-SqlAdmin -SqlServer $instance -Login sqladmin
+Reset-SqlAdmin  -SqlInstance $instance -Login sqladmin
 
 #endregion
 
 #region bits and bobs
 
 # DbaStartupParameter
-Get-DbaStartupParameter -SqlServer $instance
-Get-DbaStartupParameter -SqlServer $new
+Get-DbaStartupParameter  -SqlInstance $instance
+Get-DbaStartupParameter  -SqlInstance $new
 
 # sp_whoisactive
-Show-SqlWhoisActive -SqlServer $instance # -ShowOwnSpid -ShowSystemSpids
+Show-SqlWhoisActive  -SqlInstance $instance # -ShowOwnSpid -ShowSystemSpids
+
+#endregion
+
+#region configs
+
+# Get-DbaSpConfigure - @sirsql
+$oldprops = Get-DbaSpConfigure  -SqlInstance $old
+$newprops = Get-DbaSpConfigure  -SqlInstance $new
+
+$propcompare = foreach ($prop in $oldprops) {
+    [pscustomobject]@{
+    Config = $prop.DisplayName
+    'SQL Server 2014' = $prop.RunningValue
+    'SQL Server 2016' = $newprops | Where ConfigName -eq $prop.ConfigName | Select -ExpandProperty RunningValue
+    }
+} 
+
+$propcompare | Out-GridView
+
+# Copy-SqlSpConfigure
+Copy-SqlSpConfigure -Source $old -Destination $new -Configs DefaultBackupCompression, IsSqlClrEnabled
+
+# Get-DbaSpConfigure - @sirsql
+Get-DbaSpConfigure  -SqlInstance $new | Where-Object { $_.ConfigName -in 'DefaultBackupCompression', 'IsSqlClrEnabled' } | 
+Select-Object ConfigName, RunningValue, IsRunningDefaultValue | Format-Table -AutoSize
 
 #endregion
