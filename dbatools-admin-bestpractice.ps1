097a0077cf2f1010074caa170b8507f8bc6a6eb4
ctrlbold <clemaire@gmail.com>
2017-05-01 01:10:01 +0200

    fixes

--- dbatools-admin-bestpractice.ps1
+++ dbatools-admin-bestpractice.ps1
@@ -14,34 +14,35 @@ Import-Module C:\github\dbatools -Force
 # Set some vars
 $new = "localhost\sql2016"
 $old = $instance = "localhost"
+$allservers = $old, $new
 
 #region backuprestore
 
 Start-Process https://dbatools.io/snowball
 
 # standard
-Restore-DbaDatabase  -SqlInstance localhost -Path C:\temp\SQL2016_Cube_Query_History_FULL_20170206_115448.bak
-Restore-DbaDatabase  -SqlInstance localhost -Path C:\temp\SQL2016_Cube_Query_History_FULL_20170206_115448.bak -WithReplace
+Restore-DbaDatabase -SqlInstance localhost -Path "C:\temp\AdventureWorks2012-Full Database Backup.bak"
+Restore-DbaDatabase -SqlInstance localhost -Path "C:\temp\AdventureWorks2012-Full Database Backup.bak" -WithReplace
 
 # ola!
-Invoke-Item \\nas\sql\SQL2016\db1
-Restore-DbaDatabase  -SqlInstance sql2016 -Path \\nas\sql\SQL2016\db1 -WithReplace -DestinationDataDirectory C:\temp
+Invoke-Item \\workstation\backups\WORKSTATION\SharePoint_Config
+Restore-DbaDatabase -SqlInstance $new -Path \\workstation\backups\WORKSTATION\SharePoint_Config -WithReplace -DestinationDataDirectory C:\temp
 
-foreach ($database in (Get-ChildItem -Directory \\nas\sql\SQL2016).FullName)
+foreach ($database in (Get-ChildItem -Directory \\workstation\backups\sql2012).FullName)
 {
-    Write-Output "Processing $database"
-    Restore-DbaDatabase  -SqlInstance localhost\sql2016 -Path $database -NoRecovery # -RestoreTime (Get-date).AddHours(-3)
+  Write-Output "Processing $database"
+  Restore-DbaDatabase -SqlInstance localhost\sql2016 -Path $database -NoRecovery -RestoreTime (Get-date).AddHours(-3)
 }
 
 # What about backups?
-Get-DbaDatabase -SqlInstance sql2005 -Databases db_2005_CL80 | Backup-DbaDatabase -BackupDirectory C:\temp -NoCopyOnly
+Get-DbaDatabase -SqlInstance localhost -Databases SharePoint_Config | Backup-DbaDatabase -BackupDirectory C:\temp -NoCopyOnly
 
 # history
-Get-DbaBackupHistory  -SqlInstance sql2005 -Databases dumpsterfire4, db_2005_CL80 | Out-GridView
+Get-DbaBackupHistory -SqlInstance localhost -Databases AdventureWorks2012, SharePoint_Config | Out-GridView
 
 # backup header
-Read-DbaBackupHeader  -SqlInstance $instance -Path "\\nas\sql\SQL2016\db1\FULL\SQL2016_db1_FULL_20170206_115448.bak"
-Read-DbaBackupHeader  -SqlInstance $instance -Path "\\nas\sql\SQL2016\db1\FULL\SQL2016_db1_FULL_20170206_115448.bak" | SELECT ServerName, DatabaseName, UserName, BackupFinishDate, SqlVersion, BackupSizeMB
+Read-DbaBackupHeader -SqlInstance $instance -Path "\\workstation\backups\WORKSTATION\SharePoint_Config\FULL\WORKSTATION_SharePoint_Config_FULL_20170114_224317.bak"
+Read-DbaBackupHeader -SqlInstance $instance -Path "\\workstation\backups\WORKSTATION\SharePoint_Config\FULL\WORKSTATION_SharePoint_Config_FULL_20170114_224317.bak" | SELECT ServerName, DatabaseName, UserName, BackupFinishDate, SqlVersion, BackupSizeMB
 
 # Find it!
 Find-DbaCommand -Tag Config
@@ -52,6 +53,7 @@ Find-DbaCommand -Tag Config
 Start-Process https://dbatools.io/schwifty
 Start-Process "C:\Program Files\Microsoft\Kerberos Configuration Manager for SQL Server\KerberosConfigMgr.exe"
 
+# oh this is tough - no domain
 Get-DbaSpn | Format-Table
 $allservers | Test-DbaSpn | Out-GridView -PassThru | Set-DbaSpn -Whatif
 Get-DbaSpn | Remove-DbaSpn -Whatif
@@ -65,7 +67,7 @@ $allservers | Get-DbaLastBackup | Where-Object LastFullBackup -eq $null | Format
 $allservers | Get-DbaLastBackup | Where-Object { $_.SinceLog -gt '00:15:00' -and $_.RecoveryModel -ne 'Simple' -and $_.Database -ne 'model' } | Select Server, Database, SinceFull, DatabaseCreated | Out-GridView
 
 # LastGoodCheckDb - by @jagoop
-$checkdbs = Get-DbaLastGoodCheckDb  -SqlInstance $instance
+$checkdbs = Get-DbaLastGoodCheckDb -SqlInstance $instance
 $checkdbs
 $checkdbs | Where LastGoodCheckDb -eq $null
 $checkdbs | Where LastGoodCheckDb -lt (Get-Date).AddDays(-1)
@@ -79,12 +81,13 @@ $diskspace | Where PercentFree -lt 20
 
 #region testing backups
 
+# Did you see? SqlServer module is now in the Powershell Gallery too!
 Get-Help Test-DbaLastBackup -Online
 Import-Module SqlServer
 Invoke-Item (Get-Item SQLSERVER:\SQL\LOCALHOST\DEFAULT).DefaultFile
 
-Test-DbaLastBackup  -SqlInstance sql2005 | Out-GridView
-Test-DbaLastBackup  -SqlInstance sql2005 -Destination localhost\sql2016 -VerifyOnly | Out-GridView
+Test-DbaLastBackup -SqlInstance localhost | Out-GridView
+Test-DbaLastBackup -SqlInstance localhost -Destination localhost\sql2016 -VerifyOnly | Out-GridView
 
 #endregion
 
@@ -97,9 +100,9 @@ $allservers | Test-DbaVirtualLogFile | Where-Object {$_.Count -ge 50} | Sort-Obj
 #region databasespace
 
 # Get Db Free Space AND write it to disk
-Get-DbaDatabaseFreespace  -SqlInstance $instance
-Get-DbaDatabaseFreespace  -SqlInstance $instance -IncludeSystemDBs | Out-DbaDataTable | Write-DbaDataTable  -SqlInstance $instance -Table tempdb.dbo.DiskSpaceExample
-Get-DbaDatabaseFreespace  -SqlInstance $instance -IncludeSystemDBs | Out-DbaDataTable | Write-DbaDataTable  -SqlInstance $instance -Table tempdb.dbo.DiskSpaceExample -AutoCreateTable
+Get-DbaDatabaseFreespace -SqlInstance $instance
+Get-DbaDatabaseFreespace -SqlInstance $instance -IncludeSystemDBs | Out-DbaDataTable | Write-DbaDataTable -SqlInstance $instance -Table tempdb.dbo.DiskSpaceExample
+Get-DbaDatabaseFreespace -SqlInstance $instance -IncludeSystemDBs | Out-DbaDataTable | Write-DbaDataTable -SqlInstance $instance -Database tempdb -Table DiskSpaceExample -AutoCreateTable
 
 # Run a lil query
 Ssms.exe "C:\temp\tempdbquery.sql"
@@ -112,62 +115,64 @@ Ssms.exe "C:\temp\tempdbquery.sql"
 $allservers | Get-DbaMaxMemory
 $allservers | Test-DbaMaxMemory | Format-Table
 $allservers | Test-DbaMaxMemory | Where-Object { $_.SqlMaxMB -gt $_.TotalMB } | Set-DbaMaxMemory -WhatIf
-Set-DbaMaxMemory  -SqlInstance $instance -MaxMb 2048
+Set-DbaMaxMemory -SqlInstance $instance -MaxMb 2048
 
 # RecoveryModel
-Test-DbaFullRecoveryModel  -SqlInstance sql2005
-Test-DbaFullRecoveryModel  -SqlInstance sql2005 | Where { $_.ConfiguredRecoveryModel -ne $_.ActualRecoveryModel }
+Test-DbaFullRecoveryModel -SqlInstance localhost
+Test-DbaFullRecoveryModel -SqlInstance localhost | Where { $_.ConfiguredRecoveryModel -ne $_.ActualRecoveryModel }
 
 # Backup History!
-Get-DbaBackupHistory  -SqlInstance $instance
-Get-DbaBackupHistory  -SqlInstance $instance | Out-GridView
-Get-DbaBackupHistory  -SqlInstance $instance -Databases AdventureWorks2012 | Format-Table -AutoSize
+Get-DbaBackupHistory -SqlInstance $instance
+Get-DbaBackupHistory -SqlInstance $instance | Out-GridView
+Get-DbaBackupHistory -SqlInstance $instance -Databases AdventureWorks2012 | Format-Table -AutoSize
 
 # Restore History!
-Get-DbaRestoreHistory  -SqlInstance $instance | Out-GridView
+Get-DbaRestoreHistory -SqlInstance $instance | Out-GridView
  
 #endregion
 
 #region mindblown
 
-# Find-DbaStoredProcdure
+# Find-DbaStoredProcdure - @claudioessilva, @cl, Stephen Bennett
+# 37,545 SQL Server stored procedures on 9 servers evaluated in 8.67 seconds!
+
 $allservers | Find-DbaStoredProcedure -Pattern dbatools
 $allservers | Find-DbaStoredProcedure -Pattern dbatools | Select * | Out-GridView
-$coupleservers | Find-DbaStoredProcedure -Pattern '\w+@\w+\.\w+'
+$allservers | Find-DbaStoredProcedure -Pattern '\w+@\w+\.\w+'
 
 # Remove dat orphan - by @sqlstad
-Find-DbaOrphanedFile  -SqlInstance $instance
-((Find-DbaOrphanedFile  -SqlInstance $instance -RemoteOnly | Get-ChildItem | Select -ExpandProperty Length | Measure-Object -Sum)).Sum / 1MB
-Find-DbaOrphanedFile  -SqlInstance $instance -RemoteOnly | Remove-Item
+Find-DbaOrphanedFile -SqlInstance $instance
+((Find-DbaOrphanedFile -SqlInstance $instance -RemoteOnly | Get-ChildItem | Select -ExpandProperty Length | Measure-Object -Sum)).Sum / 1MB
+Find-DbaOrphanedFile -SqlInstance $instance -RemoteOnly | Remove-Item
 
 # Reset-SqlAdmin
-Reset-SqlAdmin  -SqlInstance $instance -Login sqladmin
+Reset-SqlAdmin -SqlInstance $instance -Login sqladmin
 
 #endregion
 
 #region bits and bobs
 
 # DbaStartupParameter
-Get-DbaStartupParameter  -SqlInstance $instance
-Get-DbaStartupParameter  -SqlInstance $new
+Get-DbaStartupParameter -SqlInstance $instance
+Get-DbaStartupParameter -SqlInstance $new
 
 # sp_whoisactive
-Show-SqlWhoisActive  -SqlInstance $instance # -ShowOwnSpid -ShowSystemSpids
+Invoke-DbaWhoisActive -SqlInstance $instance -ShowOwnSpid -ShowSystemSpids
 
 #endregion
 
 #region configs
 
 # Get-DbaSpConfigure - @sirsql
-$oldprops = Get-DbaSpConfigure  -SqlInstance $old
-$newprops = Get-DbaSpConfigure  -SqlInstance $new
+$oldprops = Get-DbaSpConfigure -SqlInstance $old
+$newprops = Get-DbaSpConfigure -SqlInstance $new
 
 $propcompare = foreach ($prop in $oldprops) {
-    [pscustomobject]@{
-    Config = $prop.DisplayName
-    'SQL Server 2014' = $prop.RunningValue
-    'SQL Server 2016' = $newprops | Where ConfigName -eq $prop.ConfigName | Select -ExpandProperty RunningValue
-    }
+  [pscustomobject]@{
+  Config = $prop.DisplayName
+  'SQL Server 2014' = $prop.RunningValue
+  'SQL Server 2016' = $newprops | Where ConfigName -eq $prop.ConfigName | Select -ExpandProperty RunningValue
+  }
 } 
 
 $propcompare | Out-GridView
@@ -176,7 +181,7 @@ $propcompare | Out-GridView
 Copy-SqlSpConfigure -Source $old -Destination $new -Configs DefaultBackupCompression, IsSqlClrEnabled
 
 # Get-DbaSpConfigure - @sirsql
-Get-DbaSpConfigure  -SqlInstance $new | Where-Object { $_.ConfigName -in 'DefaultBackupCompression', 'IsSqlClrEnabled' } | 
+Get-DbaSpConfigure -SqlInstance $new | Where-Object { $_.ConfigName -in 'DefaultBackupCompression', 'IsSqlClrEnabled' } | 
 Select-Object ConfigName, RunningValue, IsRunningDefaultValue | Format-Table -AutoSize
 
 #endregion
